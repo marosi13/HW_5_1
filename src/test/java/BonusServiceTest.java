bfdaaa91fc5c312fd9a079ff2e400c66da8536b4
marosi13 <67289775+marosi13@users.noreply.github.com>
2020-11-10 15:52:48 +0300

    modify pom.xml

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -21,6 +21,7 @@ class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1_000_000_60;
         boolean registered = true;
+
         long expected = 500;
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
